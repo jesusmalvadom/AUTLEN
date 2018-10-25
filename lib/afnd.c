b5e55d95cf0ac51a7eb9be2307228fc65b4286ce
DidiBaides <didirl6@gmail.com>
2018-10-25 13:51:44 +0200

    bien tabulado

--- lib/afnd.c
+++ lib/afnd.c
@@ -16,10 +16,7 @@ AFND * AFNDNuevo(char * nombre, int num_estados, int num_simbolos) {
 	p_afnd->num_estados = num_estados;
 	p_afnd->num_simbolos = num_simbolos;
 
-	char nombre_alfabeto[MAX_LEN_NOMBRE];
-	sprintf(nombre_alfabeto, "alf_%s", nombre);
-
-	p_afnd->alfabeto = alfabetoNuevo(nombre_alfabeto, num_simbolos);
+	p_afnd->alfabeto = alfabetoNuevo("A", num_simbolos);
 
 	if (p_afnd->alfabeto == NULL) return NULL;
 
@@ -29,6 +26,18 @@ AFND * AFNDNuevo(char * nombre, int num_estados, int num_simbolos) {
 	p_afnd->cadena_actual = palabraNueva();
 	if (p_afnd->cadena_actual == NULL) return NULL;
 
+	p_afnd->transiciones = (VectorIndices **) malloc(sizeof(VectorIndices *) * num_estados);
+
+	for(int i = 0; i < num_estados; i++) p_afnd->transiciones[i] = (VectorIndices *) malloc(num_simbolos * sizeof(VectorIndices));
+
+	for (int i=0; i<num_estados; i++){
+		for (int j=0; j<num_simbolos; j++) {
+			p_afnd->transiciones[i][j] = VectorIndicesNuevo(p_afnd->num_estados);
+		}
+	}
+
+	if(p_afnd->transiciones == NULL) return NULL;
+
 	return p_afnd;
 }
 
@@ -36,35 +45,49 @@ AFND * AFNDNuevo(char * nombre, int num_estados, int num_simbolos) {
  asociados con un autómata finito no determinista.*/
 void AFNDElimina(AFND * p_afnd) {
 	if (p_afnd == NULL) return;
-	printf("HOLA1\n");
-	fflush(stdout);
 	free(p_afnd->nombre);
-	printf("HOLA2\n");
-	fflush(stdout);
 
 	alfabetoElimina(p_afnd->alfabeto);
-	printf("HOLA3\n");
-	fflush(stdout);
 
 	for (int i = 0; i < p_afnd->num_estados; i++) {
-		printf("HOLA4_%d\n", i);
-		fflush(stdout);
 		if (p_afnd->estados[i] != NULL) estadoElimina(p_afnd->estados[i]);
 	}
-	printf("HOLA5\n");
-	fflush(stdout);
 	free(p_afnd->estados);
-	printf("HOLA6\n");
-	fflush(stdout);
 	palabraElimina(p_afnd->cadena_actual);
-	printf("HOLA7\n");
-	fflush(stdout);
 	return;
 }
 
 /*Imprime en el FILE * proporcionado como
  argumento el autómata finito proporcionado como argumento.*/
 void AFNDImprime(FILE * fd, AFND* p_afnd) {
+	if (fd == NULL || p_afnd == NULL) return;
+
+
+	fprintf(fd, "%s={\n", p_afnd->nombre);
+	fprintf(fd, "\tnum_simbolos = %d\n\n\t", p_afnd->num_simbolos);
+	alfabetoImprime(fd, p_afnd->alfabeto);
+	fprintf(fd, "\n\tnum_estados = %d\n\n\tQ={ ", p_afnd->num_estados);
+	for (int i = 0; i<p_afnd->num_estados; i++) {
+		estadoImprime(fd, p_afnd->estados[i]);
+	}
+	fprintf(fd, "}\n");
+	fprintf(fd, "\tFuncion de Transición = { \n");
+	for(int i=0; i<p_afnd->num_estados; i++) {
+		for (int j=0; j<p_afnd->num_simbolos; j++) {
+			fprintf(fd, "\n\t\tf(%s,%s)={ ", estadoNombre(p_afnd->estados[i]), alfabetoSimboloEn(p_afnd->alfabeto, j));
+			for (int k=0; k<p_afnd->num_estados; k++) {
+				if (VectorIndicesGetI(p_afnd->transiciones[i][j], k) == 1) {
+					fprintf(fd, "%s ", estadoNombre(p_afnd->estados[k]));
+				}
+			}
+			fprintf(fd, "}");
+		}
+	
+	}
+	fprintf(fd, "\n\t}");
+	fprintf(fd, "\n}");
+
+	
 	return;
 }
 
@@ -80,6 +103,7 @@ AFND * AFNDInsertaSimbolo(AFND * p_afnd, char * simbolo) {
 	}
 	return NULL;
 }
+
 /*Inserta en el conjunto de estados 
 del autómata proporcionado como primer
  argumento un nuevo estado cuyo nombre 
@@ -89,10 +113,16 @@ AFND * AFNDInsertaEstado(AFND * p_afnd, char * nombre, int tipo) {
 	int i;
 	if(p_afnd && nombre){
 		for(i = 0; i < p_afnd->num_estados; i++){
-			if(p_afnd->estados[i] == NULL) break;
+
+			if(p_afnd->estados[i] == NULL) {
+				break;
+			}
+
 		}
 		p_afnd->estados[i] = estadoNuevo(nombre, tipo);
-		if(p_afnd->estados[i] != NULL) return p_afnd;
+		if(p_afnd->estados[i] == NULL) return NULL;
+		
+		return p_afnd;
 	}
 	return NULL;
 }
@@ -116,6 +146,18 @@ AFND * AFNDInsertaTransicion(AFND * p_afnd,
                             char * nombre_simbolo_entrada,
                             char * nombre_estado_f ) {
 
+	if (p_afnd && nombre_estado_i && nombre_simbolo_entrada && nombre_estado_f) {
+		int indexSimbolo = alfabetoIndiceDeSimbolo(p_afnd->alfabeto, nombre_simbolo_entrada);
+		int indexEstadoI = AFNDGetEstadoIndice(p_afnd, nombre_estado_i);
+		int indexEstadoF = AFNDGetEstadoIndice(p_afnd, nombre_estado_f);
+
+		if (indexSimbolo == -1 || indexEstadoI == -1 || indexEstadoF == -1) {
+			return NULL;
+		}
+		
+		VectorIndicesSetI(p_afnd->transiciones[indexEstadoI][indexSimbolo], indexEstadoF);
+
+	}
 
     return NULL;
 }
@@ -125,24 +167,30 @@ AFND * AFNDInsertaTransicion(AFND * p_afnd,
  procesar) con el formato que puedes ver en 
  el ejemplo*/
 AFND * AFNDInicializaCadenaActual (AFND * p_afnd ) {
-	return NULL;
+	if(!p_afnd)return NULL;
+        
+    palabraElimina(p_afnd->cadena_actual);
+    p_afnd->cadena_actual=palabraNueva();
+        
+    return p_afnd;
 }
 
 
 void AFNDImprimeConjuntoEstadosActual(FILE * fd, AFND * p_afnd) {
-	/*int i;
-	fprintf(fd, "ACTUALMENTE EN {->");
-	for(i=0; i<p_afnd->tam_estados; i++){
-		if(p_afnd->cadena_actual[i]){
-			if(p_afnd->estados[i]->tipo == FINAL || p_afnd->estados[i]->tipo == INICIAL_Y_FINAL){
-				fprintf(fd, "%s* ", p_afnd->estados[i]->nombre);
-			}else{
-				fprintf(fd, "%s ", p_afnd->estados[i]->nombre);
-			}
+
+	int i;
+	if(fd == NULL || p_afnd == NULL){
+		printf("Error imprimiendo conjuntos estados acutal\n");
+		return;
+	}
+
+	fprintf(fd, "\nACTUALMENTE EN {->");
+	for(i = 0; i < p_afnd->num_estados; i++){
+		if(p_afnd->estados[i] != NULL){
+			estadoImprime(fd, p_afnd->estados[i]);
 		}
 	}
-	fprintf(fd, "}\n");*/
-	return;
+	fprintf(fd, "}\n");
 }
 
 void AFNDImprimeCadenaActual(FILE *fd, AFND * p_afnd) {
@@ -185,4 +233,15 @@ void AFNDProcesaEntrada(FILE * fd, AFND * p_afnd) {
  símbolo, es decir, al primero de la cadena).*/
 void AFNDTransita(AFND * p_afnd) {
 	return;
+}
+
+
+int AFNDGetEstadoIndice(AFND * p_afnd, char* nombre_estado) {
+	if (p_afnd && nombre_estado) {
+		for (int i = 0; i<p_afnd->num_estados; i++) {
+			if (estadoEs(p_afnd->estados[i], nombre_estado))
+				return i;
+		} 
+	}
+	return -1;
 }
